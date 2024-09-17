c645a724d3fcd576652a0402f44f652b877ac7ea
Matheus <mgustavocoder@gmail.com>
2024-09-17 12:08:39 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+    region = var.region
+  }
+  
+  resource "aws_security_group" "aurora_sg" {
+    name        = "aurora-security-group"
+    description = "Allow MySQL traffic"
+    vpc_id      = var.vpc_id
+  
+    ingress {
+      description = "Allow MySQL inbound"
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+    tags = {
+      Name = "aurora-security-group"
+    }
+  }
+  
+  resource "aws_db_subnet_group" "aurora_subnet_group" {
+    name       = "aurora-subnet-group"
+    subnet_ids = var.subnet_ids
+    tags = {
+      Name = "aurora-subnet-group"
+    }
+  }
+  
+  resource "aws_rds_cluster" "aurora_cluster" {
+    cluster_identifier      = var.cluster_identifier
+    engine                  = "aurora-mysql"
+    engine_version          = "8.0.mysql_aurora.3.05.2"
+    master_username         = var.master_username
+    master_password         = var.master_password
+    database_name           = "admin"
+    skip_final_snapshot     = true
+    vpc_security_group_ids  = [aws_security_group.aurora_sg.id]
+    db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
+    storage_encrypted       = true
+    backup_retention_period = 7
+    apply_immediately       = true
+  
+    tags = {
+      Name = "aurora-cluster"
+    }
+  }
+  
+  # Aurora Cluster Instance
+  resource "aws_rds_cluster_instance" "aurora_instance" {
+    identifier              = "aurora-cluster-instance-1"
+    cluster_identifier      = aws_rds_cluster.aurora_cluster.id
+    instance_class          = "db.t3.small"
+    engine                  = aws_rds_cluster.aurora_cluster.engine
+    engine_version          = aws_rds_cluster.aurora_cluster.engine_version
+    publicly_accessible     = true
+    apply_immediately       = true
+  
+    tags = {
+      Name = "aurora-cluster-instance"
+    }
+  }
+  
+  # Output the Cluster Endpoint
+  output "cluster_endpoint" {
+    value = aws_rds_cluster.aurora_cluster.endpoint
+  }
+  
+  output "reader_endpoint" {
+    value = aws_rds_cluster.aurora_cluster.reader_endpoint
+  }
+  
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the Aurora cluster and other resources will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs to associate with the Aurora cluster"
+  type        = list(string)
+}
+
+variable "cluster_identifier" {
+  description = "The identifier for the Aurora MySQL cluster"
+  type        = string
+  default     = "postech-fiap"
+}
+
+variable "master_username" {
+  description = "The master username for the Aurora MySQL cluster"
+  type        = string
+}
+
+variable "master_password" {
+  description = "The master password for the Aurora MySQL cluster"
+  type        = string
+}
