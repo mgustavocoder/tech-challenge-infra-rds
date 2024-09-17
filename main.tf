2b0eb9e287ba1ed3fc8e76dccda63b9cce4805f1
Matheus <mgustavocoder@gmail.com>
2024-09-17 13:33:46 -0300

    cluster identifier.

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ provider "aws" {
   resource "aws_rds_cluster_instance" "aurora_instance" {
     identifier              = "aurora-cluster-instance-1"
     cluster_identifier      = aws_rds_cluster.aurora_cluster.id
-    instance_class          = "db.t3.small"
+    instance_class          = "db.t3.medium"
     engine                  = aws_rds_cluster.aurora_cluster.engine
     engine_version          = aws_rds_cluster.aurora_cluster.engine_version
     publicly_accessible     = true
